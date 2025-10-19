89dc8187c401d5c0119854f694fff1e99dada51d
pubethSri <66070158@kmitl.ac.th>
2025-10-19 19:29:29 +0700

    Big thing: the Create and Delete functionality is working hooray!
    Detail:
    
    changes in ipa2024_final.py
      - import the restconf_final as module
      - uncomment and code the condition for "create" and "delete"
        - called the fucntion from the modules for each commands (create and delete)
        - use the return value to assign to responseMessage use later as the message display in Webex
      - uncomment and fill the replace the code for postData
        - postData use the responseMessage as the "text"
      - edit the requests.post at Line 134 for posting message to Webex
    
    changes in restconf_final.py
      - changes the api_url to the correct one according to ietf
      - edit the headers for using with every HTTP method
      - edit the create() for command "create"
        - edit the yangConfig to match the test details
        - create conditions for return messages that will be use with responseMessage in ipa2024_final.py
          - if it works (code 200 - 299 except 204), this mean we successfully created an interface
          - if it works but return 204, this mean the interface is already exists
          - others than those 2 options before means some thing broke
      - edit the delete() for command "delete"
        - edit the resp to use correct argument
        - create conditions for return messages like in create()
          - if it work (code 200 - 299), delete sucessfully
          - if it not work, "Oh là là Cannot delete"
    
    tested manually in Webex
    create checked
    delete checked
    next -> enable, disable and status
    then I will go shower!

--- ipa2024_final.py
+++ ipa2024_final.py
@@ -10,6 +10,8 @@ import time
 import json
 import requests
 
+# .py files
+import restconf_final
 #######################################################################################
 # 2. Assign the Webex access token to the variable ACCESS_TOKEN using environment variables.
 
@@ -67,74 +69,74 @@ while True:
 
     # check if the text of the message starts with the magic character "/" followed by your studentID and a space and followed by a command name
     #  e.g.  "/66070123 create"
-    if message.startswith("/"):
+    if message.startswith("/66070158"):
 
         # extract the command
         command = message.split(" ")[1]
         print(command)
 
-# # 5. Complete the logic for each command
+# 5. Complete the logic for each command
 
-#         if command == "create":
-#             <!!!REPLACEME with code for create command!!!>     
-#         elif command == "delete":
-#             <!!!REPLACEME with code for delete command!!!>
-#         elif command == "enable":
-#             <!!!REPLACEME with code for enable command!!!>
-#         elif command == "disable":
-#             <!!!REPLACEME with code for disable command!!!>
-#         elif command == "status":
-#             <!!!REPLACEME with code for status command!!!>
-#         elif command == "gigabit_status":
-#             <!!!REPLACEME with code for gigabit_status command!!!>
-#         elif command == "showrun":
-#             <!!!REPLACEME with code for showrun command!!!>
-#         else:
-#             responseMessage = "Error: No command or unknown command"
+        if command == "create":
+            responseMessage = restconf_final.create(); 
+        elif command == "delete":
+            responseMessage = restconf_final.delete(); 
+        # elif command == "enable":
+        #     <!!!REPLACEME with code for enable command!!!>
+        # elif command == "disable":
+        #     <!!!REPLACEME with code for disable command!!!>
+        # elif command == "status":
+        #     <!!!REPLACEME with code for status command!!!>
+        # elif command == "gigabit_status":
+        #     <!!!REPLACEME with code for gigabit_status command!!!>
+        # elif command == "showrun":
+        #     <!!!REPLACEME with code for showrun command!!!>
+        else:
+            responseMessage = "Error: No command or unknown command"
         
-# # 6. Complete the code to post the message to the Webex Teams room.
+# 6. Complete the code to post the message to the Webex Teams room.
 
-#         # The Webex Teams POST JSON data for command showrun
-#         # - "roomId" is is ID of the selected room
-#         # - "text": is always "show running config"
-#         # - "files": is a tuple of filename, fileobject, and filetype.
+        # The Webex Teams POST JSON data for command showrun
+        # - "roomId" is is ID of the selected room
+        # - "text": is always "show running config"
+        # - "files": is a tuple of filename, fileobject, and filetype.
 
-#         # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
+        # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
         
-#         # Prepare postData and HTTPHeaders for command showrun
-#         # Need to attach file if responseMessage is 'ok'; 
-#         # Read Send a Message with Attachments Local File Attachments
-#         # https://developer.webex.com/docs/basics for more detail
+        # Prepare postData and HTTPHeaders for command showrun
+        # Need to attach file if responseMessage is 'ok'; 
+        # Read Send a Message with Attachments Local File Attachments
+        # https://developer.webex.com/docs/basics for more detail
 
-#         if command == "showrun" and responseMessage == 'ok':
-#             filename = "<!!!REPLACEME with show run filename and path!!!>"
-#             fileobject = <!!!REPLACEME with open file!!!>
-#             filetype = "<!!!REPLACEME with Content-type of the file!!!>"
-#             postData = {
-#                 "roomId": <!!!REPLACEME!!!>,
-#                 "text": "show running config",
-#                 "files": (<!!!REPLACEME!!!>, <!!!REPLACEME!!!>, <!!!REPLACEME!!!>),
-#             }
-#             postData = MultipartEncoder(<!!!REPLACEME!!!>)
-#             HTTPHeaders = {
-#             "Authorization": ACCESS_TOKEN,
-#             "Content-Type": <!!!REPLACEME with postData Content-Type!!!>,
-#             }
-#         # other commands only send text, or no attached file.
-#         else:
-#             postData = {"roomId": <!!!REPLACEME!!!>, "text": <!!!REPLACEME!!!>}
-#             postData = json.dumps(postData)
+        # if command == "showrun" and responseMessage == 'ok':
+        #     filename = "<!!!REPLACEME with show run filename and path!!!>"
+        #     fileobject = <!!!REPLACEME with open file!!!>
+        #     filetype = "<!!!REPLACEME with Content-type of the file!!!>"
+        #     postData = {
+        #         "roomId": <!!!REPLACEME!!!>,
+        #         "text": "show running config",
+        #         "files": (<!!!REPLACEME!!!>, <!!!REPLACEME!!!>, <!!!REPLACEME!!!>),
+        #     }
+        #     postData = MultipartEncoder(<!!!REPLACEME!!!>)
+        #     HTTPHeaders = {
+        #     "Authorization": ACCESS_TOKEN,
+        #     "Content-Type": <!!!REPLACEME with postData Content-Type!!!>,
+        #     }
+        # # other commands only send text, or no attached file.
+        # else:
+        postData = {"roomId": roomIdToGetMessages, "text": responseMessage}
+        postData = json.dumps(postData)
 
-#             # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
-#             HTTPHeaders = {"Authorization": <!!!REPLACEME!!!>, "Content-Type": <!!!REPLACEME!!!>}   
+        # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
+        HTTPHeaders = {"Authorization": f"Bearer {ACCESS_TOKEN}", "Accept": "application/yang-data+json", "Content-Type": "application/yang-data+json"}   
 
-#         # Post the call to the Webex Teams message API.
-#         r = requests.post(
-#             "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
-#             data=<!!!REPLACEME!!!>,
-#             headers=<!!!REPLACEME!!!>,
-#         )
-#         if not r.status_code == 200:
-#             raise Exception(
-#                 "Incorrect reply from Webex Teams API. Status code: {}".format(r.status_code)
-#             )
+        # Post the call to the Webex Teams message API.
+        r = requests.post(
+            "https://webexapis.com/v1/messages",
+            data=postData,
+            headers=HTTPHeaders,
+        )
+        if not r.status_code == 200:
+            raise Exception(
+                "Incorrect reply from Webex Teams API. Status code: {}".format(r.status_code)
+            )
--- restconf_final.py
+++ restconf_final.py
@@ -2,105 +2,128 @@ import json
 import requests
 requests.packages.urllib3.disable_warnings()
 
-# Router IP Address is 10.0.15.181-184
-api_url = "<!!!REPLACEME with URL of RESTCONF Configuration API!!!>"
+# Router IP Address is 10.0.15.61
+api_url = f"https://10.0.15.61/restconf/data/ietf-interfaces:interfaces/interface=Loopback66070158"
 
 # the RESTCONF HTTP headers, including the Accept and Content-Type
 # Two YANG data formats (JSON and XML) work with RESTCONF 
-headers = <!!!REPLACEME with Accept and Content-Type information headers!!!>
+headers = {
+    "Accept": "application/yang-data+json",
+    "Content-Type": "application/yang-data+json"
+}
 basicauth = ("admin", "cisco")
 
 
 def create():
-    yangConfig = <!!!REPLACEME with YANG data!!!> 
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": "Loopback66070158",
+            "description": "66070158's Loopback created by RESTCONF",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+            "ietf-ip:ipv4": {
+                "address": [
+                    {
+                        "ip": "172.1.58.1",
+                        "netmask": "255.255.255.0"
+                    }
+                ]
+            }
+        }
+    }
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+    resp = requests.put(
+        api_url, 
+        data=json.dumps(yangConfig), 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
 
-    if(resp.status_code >= 200 and resp.status_code <= 299):
+    if((resp.status_code >= 200 and resp.status_code <= 299) and resp.status_code != 204):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "축하해! Interface loopback 66070158 is created successfully"
+    elif (resp.status_code == 204):
+        print('the interface is already existed: {}'.format(resp.status_code))
+        return "아쉽다! loopback 66070158 is already exist"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
+        return "아이구! Cannot create: Interface loopback 66070158"
 
 
 def delete():
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
+    resp = requests.delete(
+        api_url, 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "Au revoir! Interface loopback 66070158 is deleted successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
+        return "Oh là là Cannot delete: Interface loopback 66070158"
 
 
-def enable():
-    yangConfig = <!!!REPLACEME with YANG data!!!>
+# def enable():
+#     yangConfig = <!!!REPLACEME with YANG data!!!>
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
-        verify=False
-        )
+#     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
+#         <!!!REPLACEME with URL!!!>, 
+#         data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+#         auth=basicauth, 
+#         headers=headers, 
+#         verify=False
+#         )
 
-    if(resp.status_code >= 200 and resp.status_code <= 299):
-        print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
-    else:
-        print('Error. Status Code: {}'.format(resp.status_code))
-
-
-def disable():
-    yangConfig = <!!!REPLACEME with YANG data!!!>
-
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
-        verify=False
-        )
-
-    if(resp.status_code >= 200 and resp.status_code <= 299):
-        print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
-    else:
-        print('Error. Status Code: {}'.format(resp.status_code))
+#     if(resp.status_code >= 200 and resp.status_code <= 299):
+#         print("STATUS OK: {}".format(resp.status_code))
+#         return "<!!!REPLACEME with proper message!!!>"
+#     else:
+#         print('Error. Status Code: {}'.format(resp.status_code))
 
 
-def status():
-    api_url_status = "<!!!REPLACEME with URL of RESTCONF Operational API!!!>"
+# def disable():
+#     yangConfig = <!!!REPLACEME with YANG data!!!>
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
-        verify=False
-        )
+#     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
+#         <!!!REPLACEME with URL!!!>, 
+#         data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+#         auth=basicauth, 
+#         headers=headers, 
+#         verify=False
+#         )
 
-    if(resp.status_code >= 200 and resp.status_code <= 299):
-        print("STATUS OK: {}".format(resp.status_code))
-        response_json = resp.json()
-        admin_status = <!!!REPLACEME!!!>
-        oper_status = <!!!REPLACEME!!!>
-        if admin_status == 'up' and oper_status == 'up':
-            return "<!!!REPLACEME with proper message!!!>"
-        elif admin_status == 'down' and oper_status == 'down':
-            return "<!!!REPLACEME with proper message!!!>"
-    elif(resp.status_code == 404):
-        print("STATUS NOT FOUND: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
-    else:
-        print('Error. Status Code: {}'.format(resp.status_code))
+#     if(resp.status_code >= 200 and resp.status_code <= 299):
+#         print("STATUS OK: {}".format(resp.status_code))
+#         return "<!!!REPLACEME with proper message!!!>"
+#     else:
+#         print('Error. Status Code: {}'.format(resp.status_code))
+
+
+# def status():
+#     api_url_status = "<!!!REPLACEME with URL of RESTCONF Operational API!!!>"
+
+#     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
+#         <!!!REPLACEME with URL!!!>, 
+#         auth=basicauth, 
+#         headers=headers, 
+#         verify=False
+#         )
+
+#     if(resp.status_code >= 200 and resp.status_code <= 299):
+#         print("STATUS OK: {}".format(resp.status_code))
+#         response_json = resp.json()
+#         admin_status = <!!!REPLACEME!!!>
+#         oper_status = <!!!REPLACEME!!!>
+#         if admin_status == 'up' and oper_status == 'up':
+#             return "<!!!REPLACEME with proper message!!!>"
+#         elif admin_status == 'down' and oper_status == 'down':
+#             return "<!!!REPLACEME with proper message!!!>"
+#     elif(resp.status_code == 404):
+#         print("STATUS NOT FOUND: {}".format(resp.status_code))
+#         return "<!!!REPLACEME with proper message!!!>"
+#     else:
+#         print('Error. Status Code: {}'.format(resp.status_code))
