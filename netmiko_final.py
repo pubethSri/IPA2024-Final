333061d812d169e51302a4ad6e55d67711d21f32
pubethSri <66070158@kmitl.ac.th>
2025-10-20 12:43:52 +0700

    I gave up, will test at คณะ later

--- netmiko_final.py
+++ netmiko_final.py
@@ -1,12 +1,12 @@
 from netmiko import ConnectHandler
 from pprint import pprint
 
-device_ip = "<!!!REPLACEME with router IP address!!!>"
+device_ip = "10.0.15.65"
 username = "admin"
 password = "cisco"
 
 device_params = {
-    "device_type": "<!!!REPLACEME with device type for netmiko!!!>",
+    "device_type": "cisco_ios",
     "ip": device_ip,
     "username": username,
     "password": password,
@@ -15,20 +15,24 @@ device_params = {
 
 def gigabit_status():
     ans = ""
+    interfaces_status = []
     with ConnectHandler(**device_params) as ssh:
         up = 0
         down = 0
         admin_down = 0
-        result = ssh.send_command("<!!!REPLACEME with proper command!!!>", use_textfsm=True)
+        result = ssh.send_command("show ip interface brief", use_textfsm=True)
         for status in result:
-            if <!!!Write code here!!!>:
-                <!!!Write code here!!!>
-                if <!!!Write code here!!!> == "up":
+            if status['interface'] in ['GigabitEthernet1', 'GigabitEthernet2', 'GigabitEthernet3', 'GigabitEthernet4']:
+                interfaces_status.append(f"{status['interface']} {status['status']}")
+                if status['status'] == "up":
                     up += 1
-                elif <!!!Write code here!!!> == "down":
+                elif status['status'] == "down":
                     down += 1
-                elif <!!!Write code here!!!> == "administratively down":
+                elif status['status'] == "administratively down":
                     admin_down += 1
-        ans = <!!!Write code here!!!>
+        ans = ", ".join(interfaces_status)
+        ans += f" -> {up} up, {down} down, {admin_down}, administratively down"
         pprint(ans)
         return ans
+
+gigabit_status()
